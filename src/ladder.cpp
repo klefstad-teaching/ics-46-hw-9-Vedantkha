774ed9d05e32881fe7eab22aeda7bc3ab2675155
Vedantkha <khatriv1@uci.edu>
2025-03-14 11:44:53 +0000

    carred a fduncti0on for the edit the distanmce wthin for the l;adder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -16,3 +16,28 @@ void load_words(set<string> &word_list, const string &file_name) {
     word_list.insert(word);
   }
 }
+
+bool edit_distance_within(const string &str1, const string &str2, int d) {
+  int n = str1.size();
+  int m = str2.size();
+  if (abs(n - m) > d) {
+    return false;
+  }
+  vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+  for (int i = 0; i <= n; i++) {
+    dp[i][0] = i;
+  }
+  for (int j = 0; j <= m; j++) {
+    dp[0][j] = j;
+  }
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      if (str1[i - 1] == str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+      }
+    }
+  }
+  return dp[n][m] <= d;
+}
