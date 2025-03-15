d901f87cee771d8c0256862cbc9ff19fc54fbc26
Vedantkha <khatriv1@uci.edu>
2025-03-15 20:32:04 +0000

    assign used

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -6,7 +6,7 @@ using namespace std;
 vector<int> dijkstra_shortest_path(const Graph &G, int source, vector<int> &previous) {
   vector<int> distances(G.numVertices, INF);
   distances[source] = 0;
-  previous.resize(G.numVertices, -1);
+  previous.assign(G.numVertices, -1);
   priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> Q;
   Q.push(make_pair(0, source));
   
