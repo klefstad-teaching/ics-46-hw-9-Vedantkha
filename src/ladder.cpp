6ce94cad85bd205a31b49cda64e67ee6b30f43b8
Vedantkha <khatriv1@uci.edu>
2025-03-15 05:48:04 +0000

    trying to fix te print ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -165,16 +165,14 @@ vector<string> generate_word_ladder(const string &begin_word,
   return {};
 }
 
+
 void print_word_ladder(const vector<string> &ladder) {
   if (ladder.empty()) {
     cout << "No word ladder found." << endl;
   } else {
-    cout << "Word ladder found: ";  
-    for (size_t i = 0; i < ladder.size(); i++) {
-      cout << ladder[i];
-      if (i < ladder.size() - 1) {
-        cout << " ";
-      }
+    cout << "Word ladder found: ";
+    for (const string &word : ladder) {
+      cout << word << " ";
     }
     cout << endl;
   }
